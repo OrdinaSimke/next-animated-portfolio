11ee4f474cb84128a0cd6c9538cef810f065d3a8
simke <simke.nys@gmail.com>
2024-02-13 15:10:45 +0100

    font sizes

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -48,7 +48,7 @@ const Portfolio = () => {
         ref={ref}
         // style={{ scrollbarWidth: 'thin' }}
       >
-        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+        <div className="w-screen h-[calc(100vh-16rem)] flex items-center justify-center text-4xl sm:text-8xl text-center">
           My works
         </div>
         <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
@@ -92,29 +92,28 @@ const Portfolio = () => {
       <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
         <h1>Do you have a project?</h1>
         <div className="relative">
-          <motion.svg
-            animate={{ rotate: 360 }}
-            transition={{ duration: 8, ease: 'linear', repeat: Infinity }}
-            viewBox="0 0 300 300"
-            className="w-64 h-64 md:w-[500px] md:h-[500px] "
-          >
-            <defs>
-              <path
-                id="circlePath"
-                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
-              />
-            </defs>
-            <text fill="#000">
-              <textPath xlinkHref="#circlePath" className="text-xl">
-                Front-end Developer and UI Designer
-              </textPath>
-            </text>
-          </motion.svg>
-          <Link
-            href="/contact"
-            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center"
-          >
-            Hire Me
+          <Link href="/contact">
+            <motion.svg
+              animate={{ rotate: 360 }}
+              transition={{ duration: 8, ease: 'linear', repeat: Infinity }}
+              viewBox="0 0 300 300"
+              className="w-64 h-64 md:w-[500px] md:h-[500px] "
+            >
+              <defs>
+                <path
+                  id="circlePath"
+                  d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+                />
+              </defs>
+              <text fill="#000">
+                <textPath xlinkHref="#circlePath" className="text-xl">
+                  Front-end Developer and UI Designer
+                </textPath>
+              </text>
+            </motion.svg>
+            <div className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full flex items-center justify-center">
+              Hire Me
+            </div>
           </Link>
         </div>
       </div>
